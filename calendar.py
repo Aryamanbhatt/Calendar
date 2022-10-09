ac34c93d0739a6341c8812bd35391c9ea47f2268
ARYAMAN BHATT <104625114+Aryamanbhatt@users.noreply.github.com>
2022-10-09 12:20:21 +0530

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -1,10 +1,8 @@
 import calendar  
 # Enter the month and year  
 yy = int(input("Enter year: "))  
-mm = int(input("Enter month: "))  
+mm = int(input("Enter month in number: "))  
   
 # display the calendar  
 print(calendar.month(yy,mm))
-print("thank you")
-print("thank you")
 print("thank you")
